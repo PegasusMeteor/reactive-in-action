032fb7d63d4be45a3a749abdf00be08bd96c2de1
quliang <quliang@growingio.com>
2020-09-04 13:34:13 +0800

    init project

---
+++ src/main/java/tech/selinux/reactor/ReactorExampleApplication.java
@@ -0,0 +1,13 @@
+package tech.selinux.reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/selinux/reactor/ReactorExampleApplicationTests.java
@@ -0,0 +1,14 @@
+package tech.selinux.reactor;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
