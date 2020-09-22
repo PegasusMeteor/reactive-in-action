5e58da6e30735504fe17bf1aaa4a507a1f0f7c38
quliang <quliang@growingio.com>
2020-09-22 15:46:33 +0800

    update modules

--- reactor/src/test/java/tech/selinux/reactor/MonoTest.java
+++ reactor/src/test/java/tech/selinux/reactor/MonoTest.java
@@ -1,10 +1,12 @@
 package tech.selinux.reactor;
 
+import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class MonoTest {
 
+	@Test
 	public void monoTest01(){
 		Mono<String> noData = Mono.empty();
 
